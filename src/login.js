47ecbe9f74013899ba3b018e3d59afda3c5f8835
Koe1539 <Koe1539@naver.com>
2022-12-22 02:17:42 +0900

    add

--- src/login.js
+++ src/login.js
@@ -6,9 +6,11 @@ const loginBtn = document.getElementById('LOGIN_BTN');
 
 function color() {
   if (loginId.value.length > 0 && loginPw.value.length >= 5) {
+    loginBtn.style.cursor = 'pointer';
     loginBtn.style.backgroundColor = '#1a1b1c';
     loginBtn.disabled = false;
   } else {
+    loginBtn.style.cursor = '';
     loginBtn.style.backgroundColor = '#8b8b8b';
     loginBtn.disabled = true;
   }
@@ -16,6 +18,12 @@ function color() {
 
 function moveToMain() {
   location.href = './index.html';
+}
+
+function enterKey() {
+  if (window.event.keyCode == 13 && loginPw.value.length >= 5) {
+    moveToMain();
+  }
 }
 
 loginId.addEventListener('keyup', color);
